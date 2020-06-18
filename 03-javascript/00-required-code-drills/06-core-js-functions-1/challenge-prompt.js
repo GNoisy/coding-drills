c96113bd9b886bebe4753717f2db7c4f6d7126de
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-18 08:48:23 -0700

    finished drills 04 - 09

--- 03-javascript/00-required-code-drills/06-core-js-functions-1/challenge-prompt.js
+++ 03-javascript/00-required-code-drills/06-core-js-functions-1/challenge-prompt.js
@@ -1,5 +1,7 @@
 // define a function "fun" that console logs "Functions are FUN!"
-
+function fun() {
+    console.log("Functions are fun");
+}
 
 
 // before moving on, open index.html and see what console logs have been made.
@@ -7,66 +9,93 @@
 /*
 
 Your Answer here
-
+haven't invoked the function
 */
 
 
 // define a function "thirtySeven" that console logs the sum of 18 and 19
-
+function thirtySeven() {
+    console.log(18 + 19);
+}
 
 
 // call two functions you've defined so far
-
+fun();
+thirtySeven();
 
 // define a function "log" that takes in an argument and console logs that argument
-
+function log(x) {
+    console.log(x);
+}
 
 
 // define a function "hello" that calls on the "log" function to console log "Hello World"
-
+function hello() {
+    log("Hello World");
+}
 
 
 // call your "hello" function
-
+hello();
 
 // define a function "popUp" that creates an alert that says "This is an alert."
 
-
+function popUp() {
+    alert("This is an alert");
+}
 
 // define a function "noPopUp" that creates an alert that says "Okay, you won't get a pop up."
-
+function noPopUp() {
+    alert("Okay, you won't get a pop up.");
+}
 
 
 // define a function "goodDay" that creates a confirm dialogue asking the user if they are having a nice day
 // and uses your "log" function to display the response
-
+function goodDay() {
+    var niceDay = prompt("Are you having a nice day?");
+    log(niceDay);
+}
 
 
 // call your "goodDay" function
-
+goodDay();
 
 // define a function "userPopUp" that creates a confirm dialogue asking the user if they would like a popup
 // if they answer yes, call upon your "popUp" function; otherwise, call upon your "noPopUp" function
-
+function userPopUp() {
+    var wouldLike = confirm("Would you like a popup?");
+    if (wouldLike) {
+        popUp();
+    } else {
+        noPopUp();
+    }
+}
 
 
 // call your "userPopUp" function
-
+userPopUp();
 
 // define a function "userName" that prompts the user for their name and then 
 // uses your "log" function to display the response
-
+function userName() {
+    var yourName = prompt("What's your name?");
+    log(yourName);
+}
 
 
 // call your "userName" function
-
+userName();
 
 // define a function "favoriteFruit" that prompts the user for their favorite fruit
 // and the creates an alert that displays the response
-
+function favoriteFruit () {
+    var userFave = prompt("What's your favorite fruit?");
+    alert(userFave);
+}
 
 
 // call your "favoriteFruit" function
-
+favoriteFruit();
 
 
--- 03-javascript/00-required-code-drills/07-core-js-functions-2/challenge-prompt.js
+++ 03-javascript/00-required-code-drills/07-core-js-functions-2/challenge-prompt.js
@@ -1,45 +1,54 @@
 // define a function "warble" that takes in a string as an argument,
 // appends " arglebargle" to the end of it, and returns the result
-
-
+function warble(str) {
+    var result = str + " arglebargle";
+    return result;
+  }
 
 
 // console log the result of calling your "warble" function with the argument "hello"
-
-
+var result = warble("hello");
+// console.log(warble("hello"));
+console.log(result);
 
 // define a function "wibble" that takes a string as an argument,
 // console logs the argument, prepends "wibbly " to the argument, and returns the result
-
+function wibble(str) {
+    return "wibbly" + str;
+}
 
 
 
 // console log the result of calling your "wibble" function with the argument "bibbly"
-
+console.log(wibble("bibbly"));
 
 
 
 // define a function "consoleSum" that takes in two numbers as arguments
 // and console logs the sum of those two numbers
-
+function consoleSum(arg1, arg2) {
+    console.log(arg1 + arg2);
+}
 
 
 
 // testing your function - un-comment the following two lines when you have finished your function
 
-// console.log("The following should be 5");
-// consoleSum(2, 3);
+console.log("The following should be 5");
+consoleSum(2, 3);
 
 // define a function "returnSum" that takes in two numbers as arguments
 // and returns the sum of those two numbers
-
+function returnSum(arg1, arg2) {
+    return arg1 + arg2;
+}
 
 
 
 // testing your function - un-comment the following two lines when you have finished your function
 
-// console.log("The following should be 12");
-// console.log(returnSum(5, 7));
+console.log("The following should be 12");
+console.log(returnSum(5, 7));
 
 // using either "consoleSum" or "returnSum" and no mathematical operators,
 // define a function that takes in 3 arguments and finds the sum of those 3 numbers
@@ -53,10 +62,16 @@
 // saves the result of calling your "wibbly" function with that argument,
 // saves the result of calling your "warble" function with that argument,
 // concatenates the two results together and returns that string
-
+function danceParty(str) {
+    alert("Dance!");
+    var result1 = wibble(str);
+    var result2 = warble(str);
+    var value = result1 + " " + result2;
+    return value;
+}
 
 
 
 // console log the result of calling your "danceParty" function with your name as the argument
-
+console.log(danceParty("Gio"));
 
